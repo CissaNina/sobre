0ab13de9de9f1abd5ad1b1f5db73b8948b9a7f30
vainawebcpx <vainawebcpx@gmail.com>
2017-11-24 17:34:56 -0200

    sobre

---
+++ src/components/Hello.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <div id="coleta">
+      <h2>Sobre</h2>
+        <p>O ReciclAção propõe estimular e mobilizar a comunidade a
+        descartar adequadamente seus materiais recicláveis,
+        retirando-os do meio ambiente e dando origem a um ciclo fértil
+        e produtivo que retorna em forma de recursos locais, com
+        ênfase na educação e na cooperação</p>
+        <p>O Projeto reciclação começou 3 anos após o desabamento do
+        campinho, na Rua Gomes Lopes em 2010, provocado pelo
+        temporal e acúmulo de lixo nas encostas.36 pessoas morreram
+        e 7 mil ficaram desabrigadas. A partir disso o grupo PROA,
+        junto com a Cris dos Prazeres e sua equipe de trabalho, criaram
+        o projeto ReciclAção para promover ações de conservação e
+        conscientização ambiental no morro dos Prazeres,
+        concentradas no bem-estar das pessoas através do combate de
+        doenças como dengue, zica e chikungunya e outras doenças
+        causadas por acúmulo de lixo.</p>
+        <img src="../assets/01.jpg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
